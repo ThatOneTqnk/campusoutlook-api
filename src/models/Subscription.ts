6c7c4e8061d7fec246a9c2e23ab4f94b04086a1d
yikes <nodeadbatteries@gmail.com>
2020-02-15 16:24:58 -0600

    rename queries

---
+++ src/models/Subscription.ts
@@ -0,0 +1,10 @@
+import { IResolverObject } from "graphql-tools";
+import OutlookEvents from '../db/models/events';
+
+let resolvers : IResolverObject = {
+    // eventFeed: {
+    //     subscribe: 
+    // }
+};
+
+export default resolvers;
